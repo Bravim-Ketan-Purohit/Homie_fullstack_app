32a8407b8be409997677b3fbaa970c3aff377559
Bravim-Ketan-Purohit <bravimpurohit1305@gmail.com>
2023-04-30 00:14:14 +0530

    added edit listing functionality

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,14 @@ import Offers from './pages/Offers'
 import ForgotPassword from "./pages/ForgotPassword";
 import SignIn from "./pages/SignIn";
 import CreateListing from './pages/CreateListing';
-// import SignOut from "./pages/SignOut";
+
 import Profile from "./pages/Profile";
 import Header from './components/Header';
 import SignUp from './pages/SignUp';
 import PrivateRoute from './components/PrivateRoute';
 import { ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
+import EditListing from './pages/EditListing';
 
 
 const App = () => {
@@ -24,12 +25,15 @@ const App = () => {
           <Route path="/Profile" element={<PrivateRoute />}>
             <Route path="/Profile" element={<Profile />}></Route>
           </Route>
-          <Route path="/offers" element={<Offers />} /> 
+          <Route path="/offers" element={<Offers />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/create-listing" element={<PrivateRoute />}>
             <Route path="/create-listing" element={<CreateListing />} />
+          </Route>
+          <Route path="edit-listing" element={<PrivateRoute />}>
+            <Route path="/edit-listing/:listingId" element={<EditListing />} />
           </Route>
           <Route path="/" element={<Profile />} />
         </Routes>
