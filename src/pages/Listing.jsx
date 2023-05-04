9f74c05561b131d48cae7eb89b5307f97d276c0b
Bravim-Ketan-Purohit <bravimpurohit1305@gmail.com>
2023-05-04 12:15:15 +0530

    added share copy functionality

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -11,12 +11,14 @@ import SwiperCore, {
   Pagination,
 } from "swiper";
 import "swiper/css/bundle";
+import {BsFillShareFill} from 'react-icons/bs'
 
 const Listing = () => {
 const params = useParams();  
 const [listing,setListing] = useState(null)
 const [loading,setLoading]= useState(true)
 SwiperCore.use([Autoplay, Navigation, Pagination]);
+const [shareLinkCopied,setShareLinkCopied] = useState(false);
 useEffect(() => {
   async function fetchListing() {
     const docRef = doc(db, "listings", params.listingID);
@@ -58,6 +60,19 @@ if(loading){
             </SwiperSlide>
           ))}
         </Swiper>
+        <div
+          className="fixed top-[13%] right-[3%] z-50 bg-white cursor-pointer border-2 border-gray-400 rounded-full w-12 h-12 flex justify-center items-center"
+          onClick={() => {
+            navigator.clipboard.writeText(window.location.href);
+            setShareLinkCopied(true)
+            setTimeout(()=>{
+              setShareLinkCopied(false)
+            },2000)
+          }}
+        >
+          <BsFillShareFill className="text-xl text-slate-800" />
+        </div>
+        {shareLinkCopied && (<p className='fixed top-[23%] right-[5%] font-semibold border-2 border-gray-400 rounded-md bg-green-200 z-50 p-2  '>link copied !</p>)}
       </main>
     );
 }
