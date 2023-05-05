adcc7ca9a2457dafde0a2c9a0dbf93a4dbc138c8
Bravim-Ketan-Purohit <bravimpurohit1305@gmail.com>
2023-05-05 14:09:44 +0530

    fixed a buf of offers page

--- src/pages/Offers.jsx
+++ src/pages/Offers.jsx
@@ -104,7 +104,12 @@ export default function Offers() {
           )}
         </>
       ) : (
-        <p>There are no current offers</p>
+        <p>
+          There are no current{" "}
+          {params.categoryName === "rent"
+            ? "places for rent"
+            : "places for sale"}
+        </p>
       )}
     </div>
   );
